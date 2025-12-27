8d178a0ae4e6fc6ddf8532cf728ad1eb4a0f4dcb
Lukas Runt <lukas.runt@seznam.cz>
2025-12-27 22:03:46 +0100

    Fixing bugs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,37 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'  
-import { defineConfig } from 'vite'  
-import vue from '@vitejs/plugin-vue'  
-import vueDevTools from 'vite-plugin-vue-devtools'  
-  
-export default defineConfig({  
-  plugins: [  
-    vue(),  
-    vueDevTools(),  
-  ],  
-  resolve: {  
-    alias: {  
-      '@': fileURLToPath(new URL('./src', import.meta.url))  
-    },  
-  },  
-  server: {  
-    host: '0.0.0.0',  
-    port: 5173,  
-    strictPort: true,  
-    hmr: {  
-      protocol: 'ws',  
-      host: 'localhost', 
-      port: 5173,  
-      clientPort: 5173,  
-    },  
-    proxy: {  
-      '/api': {  
-        target: 'http://backend:8000',  
-        changeOrigin: true,  
-        rewrite: (path) => path.replace(/^\/api/, '')  
-      }  
-    },  
-    watch: {  
-      usePolling: true,  
-    }  
-  }  
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+
+  base: '/',
+
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
 })
